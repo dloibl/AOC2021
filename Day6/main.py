134f9f71ef3564bd5c437f5f983a8598d55250e2
Daniel Loibl <daniel.loibl@gmail.com>
2021-12-07 21:23:35 +0100

    solve day 6

--- Day6/main.py
+++ Day6/main.py
@@ -2,30 +2,18 @@ data = open("input.txt", "r").readlines()[0]
 
 lantern_fishes = [int(i) for i in data.split(",")]
 
-
-def calc_next_iteration(list):
-    number_fishes = len(list)
-    for index in range(0, number_fishes):
-        fish_timer = list[index]
-        if fish_timer == 0:
-            list.append(8)
-        list[index] = 6 if fish_timer == 0 else fish_timer-1
-
-    return list
+lantern_fishes_count = [lantern_fishes.count(i) for i in range(0,8)]
 
 
-def count_fished_exp(population, for_day=80):
-    for day in range(0, for_day - (for_day % 7), 7):
-        new_borns = [(i+2) % 9 for i in population if i < 7]
-        population = [i % 7 for i in population]
-        population.extend(new_borns)
+def count_fished_exp(count_list, for_day=80):
+    for day in range(0, day):
+        old = count_list
+        for index, count in enumerate(count_list):
+            count_list[index] = old[(index+1) % 9]
 
-    for day in range(0, for_day % 7):
-        population = calc_next_iteration(population)
-
-    return population
+    return sum(count_list)
 
 
-print(f"The answer to part 1 is {len(count_fished_exp(lantern_fishes, 80))}")
+print(f"The answer to part 1 is {(count_fished_exp(lantern_fishes_count, 80))}")
 
-print(len(count_fished_exp(lantern_fishes, 256)))
+print(f"The answer to part 2 is {(count_fished_exp(lantern_fishes_count, 256))}")
