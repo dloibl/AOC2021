59aa9c6d47d040d83123f3bedab4dfbc9ab0f2d0
Daniel Loibl <daniel.loibl@gmail.com>
2021-12-10 21:18:02 +0100

    solve

--- Day9/main.py
+++ Day9/main.py
@@ -4,12 +4,9 @@ m = len(data)
 n = len(data[0])-1
 low_points = []
 
-print(m)
-print(n)
-
 for x in range(0,m):
     for y in range(0,n):
-        print(data[x][y])
+      
         val = int(data[x][y])
         neighbors = []
         if x == 0 and y == 0:
@@ -32,12 +29,8 @@ for x in range(0,m):
             neighbors = [data[x-1][y], data[x+1][y], data[x][y-1], data[x][y+1]]
 
         neighbors = [int(i) for i in neighbors]
-    
-        print(neighbors)
    
         if val < min(neighbors):
             low_points.append(val+1)
-
-print(low_points)
 
 print(f"The answer to part 1 is {sum(low_points)}")
