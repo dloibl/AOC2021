64bebe4964ed8bd63a058ac8f5b4c09dbb8f3920
Daniel Loibl <daniel.loibl@gmail.com>
2021-12-12 08:26:53 +0100

    test

---
+++ Day12/main.py
@@ -0,0 +1,33 @@
+data = open("input.txt", "r").readlines()
+
+graph = {}
+
+for edge in data:
+    [from,to] = edge.split("-")
+    if node[from] is None:
+        graph[from] = [to]
+    else:
+        graph[from].append(to)
+
+print(graph)
+
+visited = set()
+
+def is_small(cave):
+    return cave == cave.lower()
+
+def traverse(visited, graph, cave, count):
+    if cave == "end":
+        count += 1
+    if is_small(cave) and not cave in visited:
+        visited.add(cave)
+        print("visited",visited)
+        for neighbour in graph[cave]:
+            traverse(visited, graph, neighbour, count)
+    if not is_small(cave):
+        for neighbour in graph[cave]:
+            traverse(visited, graph, neighbour, count)
+    return count        
+
+
+print(traverse(visited, graph, "start", 0))       
